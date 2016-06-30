e01fc4a075001e629ab6a7c37f19e9f2f7756fdc
claire <eeceb@leeds.ac.uk>
2016-06-30 15:47:09 +0100

    first commit

---
+++ mod_tests.py
@@ -0,0 +1,8 @@
+# This is for you to fill in!
+from mean import *
+
+def test_mean_1234():
+    values=[1.,2.,3.,4.]
+    calc_mean= mean(values)
+    expected_mean=2.5
+    assert expected_mean==calc_mean
